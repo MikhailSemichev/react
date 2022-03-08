39e9d87ba30520068a8cca999dac70b2e8e75caa
MOSCOW\Mikhail_Semichev <Mikhail_Semichev@epam.com>
2022-03-08 15:10:01 +0300

    Home test

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,7 @@ import React from 'react'; // we need this to make JSX compile
 export const Home = (props: unknown) => {
   return (
     <div>
-      <div>Home</div>
+      <h1>Home Test</h1>
       <code>{JSON.stringify(props)}</code>
     </div>
   );
